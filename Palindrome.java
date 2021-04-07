8cddcb026344b0e0afc23f3586169f11055826f0
RaniaHerba <>
2021-04-07 19:41:23 +0000

    j'ai essayé de developper deux codes differents mais toujours le meme probleme de configuration.

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,43 @@
 public class Palindrome{
     public static boolean is_palindrome(String s)
     {
-        // add your code here
+	int a = 1;
+   if (s.equals(""))
+   {
+	   return true;
+   }
+	  else
+	  {
+			 for (int i = 0;i < s.length();i++)
+			 {
+		 s = tangible.StringFunctions.changeCharacter(s, i, Character.toLowerCase(s.charAt(i)));
+
+			 }
+
+	   for (int i = 0;i < s.length() / 2;i++)
+
+	   {
+
+			if (s.charAt(i) == s.charAt(s.length() - i - 1))
+			{
+			   a = 1;
+			}
+	   else if (s.charAt(i) != s.charAt(s.length() - i - 1))
+	   {
+			a = 0;
+		  break;
+	   }
+	   }
+		if (a == 0)
+		{
+	return false;
+		}
+	 else
+	 {
+		 return true;
+	 }
+	  }
+
         return true;
     }
 }
